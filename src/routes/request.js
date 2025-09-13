8305f9268ac21ffb3d570fd371ca43bf629bcaf4
Pranshu-15 <pranshu923@gmail.com>
2025-09-14 03:03:40 +0530

    send connection request API Practice

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,15 +1,47 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/userAuth");
+const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const requestRouter = express.Router();
 
-requestRouter.get("/sendConnectionRequest", userAuth, (req,res) => {
+requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req,res) => {
   try{
-    const user = req.user
-    if(!user){
-      throw new Error ("Cannot send a connection request. Please login first")
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
+
+    const allowedStatus = ["intrested", "ignored"]
+    if(!allowedStatus.includes(status)){
+      return res.status(400).send("Invalid Status Type " + status)
     }
-    console.log("Sending connection Request")
-    res.send(user.firstName + " " + user.lastName + " sent a connection request successfully")
+
+    if(fromUserId.toString() === toUserId){
+      return res.status(400).json({message:"You cannot send a connection request to yourself"})
+    }
+    const toUser = await User.findById(toUserId);
+    if(!toUser){
+      return res.status(404).json({message:"Sending Request to an Invalid User"})
+    }
+    const existingConnectionRequests = await ConnectionRequest.findOne({
+      $or: [
+        {fromUserId, toUserId},
+        {fromUserId:toUserId, toUserId:fromUserId}
+      ]
+    })
+    if(existingConnectionRequests){
+      return res.status(400).json({message: "Connection Request Already exists"})
+      }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    })
+    const connectionRequestData = await connectionRequest.save()
+    res.json({
+      message:"Connection Request Sent Successfully",
+      connectionRequestData,
+    })
   }catch(err){
     res.status(400).send("Cannot send connection request due to " + err.message)
   }
