95ea167d3d29635dd04a40c06d955882908f76e9
Pranshu-15 <pranshu923@gmail.com>
2025-09-17 21:39:41 +0530

    review connection request API Practice

--- src/routes/request.js
+++ src/routes/request.js
@@ -47,4 +47,28 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async(req,res) =
   }
 })
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async(req,res) => {
+  try{
+    const loggedInUser = req.user;
+    const{status, requestId} = req.params;
+    const allowedStatus = ["accepted", "rejected"]
+    if(!allowedStatus.includes(status)){
+      return res.status(400).json({message:"Invalid status"})
+    }
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id:requestId,
+      toUserId:loggedInUser._id,
+      status: "intrested"
+    })
+    if(!connectionRequest){
+      return res.status(404).json({message: "No such connection request exists"})
+    }
+    connectionRequest.status = status
+    const data = await connectionRequest.save()
+    res.json({message: "Connection request " + status + " successfully"},data)
+  }catch(err){
+    res.status(400).send("ERROR " + err.message)
+  }
+})
+
 module.exports = requestRouter
